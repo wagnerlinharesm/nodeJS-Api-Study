864beeb1a187bbc7be61d73a9d61538db7848f84
Wagner Linhares <wagnerlink@gmail.com>
2020-03-05 21:38:41 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cores');
+
+// Iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    {useUnifiedTopology: true}
+);
+
+requireDir('./src/models');
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+
+app.listen(3001); 
